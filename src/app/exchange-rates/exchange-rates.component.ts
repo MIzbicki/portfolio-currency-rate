8fb020f6172f49f28a698c402fb6f672a2a24917
MichalIzb <michalizb@wp.pl>
2023-03-25 00:25:53 +0100

    two variables added

--- src/app/exchange-rates/exchange-rates.component.ts
+++ src/app/exchange-rates/exchange-rates.component.ts
@@ -1,29 +1,33 @@
 import { ExchangeRatesService } from './../services/exchange-rates.service';
-import { Component, OnInit, Pipe } from '@angular/core';
+import { Component, OnDestroy, OnInit, Pipe } from '@angular/core';
 
 @Component({
   selector: 'app-exchange-rates',
   templateUrl: './exchange-rates.component.html',
   styleUrls: ['./exchange-rates.component.scss']
 })
-export class ExchangeRatesComponent implements OnInit {
+export class ExchangeRatesComponent implements OnInit, OnDestroy{
 
   constructor(private service: ExchangeRatesService) { }
 
   allCurrencyRates: any = "";
+  filteredRates: rate[] = [];
   rates: rate[] = [];
+  subscription: any;
 
   ngOnInit(): void {
-    this.service.getAll()
+    this.subscription = this.service.getAll()
     .subscribe(
       response => {
         this.allCurrencyRates = response;
-        this.allCurrencyRates = this.allCurrencyRates[0];
-        console.log(this.allCurrencyRates);
+        this.filteredRates = this.allCurrencyRates = this.allCurrencyRates[0];
         this.rates = this.allCurrencyRates.rates;
-        console.log(this.rates);
       }
     )
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe;
   }
 
   filter(q: string){
